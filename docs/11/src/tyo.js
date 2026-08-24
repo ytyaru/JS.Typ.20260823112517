48c15a46fd13ce08e2534635967d16ce3a880988
ytyaru <yttry0@gmail.com>
2026-08-24 11:59:48 +0900

    add 11

---
+++ docs/11/src/tyo.js
@@ -0,0 +1,156 @@
+import {Tys} from './tys.js';
+// Type Object is/error
+export class Tyo {
+    static get is() {return Tyois}
+    static get er() {return Tyoer}
+}
+class Tyois {
+    static some(v) {
+        const N = Tys.name(v);
+        return ['Function','Method'].some(n=>N.endsWith(n)) || ['PlainObject','Array'].some(n=>n===N) || ['Descriptor','Class','Instance'].some(n=>N.startsWith(n+'<'));
+    }
+    static obj(v) {return 'PlainObject'===Tys.name(v)}
+    static ary(v) {return Array.isArray(v)}
+    static cls(v) {return Tys.name(v).startsWith('Class<')}
+    static ins(v,C) {return Tys.name(v).startsWith('Instance<') && (C ? v instanceof C: true);}
+    static get des() {return TyoisDes}
+    static get fn() {return TyoisFn}
+    static get md() {return TyoisMd}
+}
+/*
+class TyoisDes {
+    static some(v) {return Tys.name(v).startsWith('Descriptor<')}
+    static v(v) {return 'Descriptor<Value>'===Tys.name(v)}
+    static m(v) {return 'Descriptor<Method>'===Tys.name(v)}
+    static g(v) {return 'Descriptor<Getter>'===Tys.name(v)}
+    static s(v) {return 'Descriptor<Setter>'===Tys.name(v)}
+    static a(v) {return 'Descriptor<Accessor>'===Tys.name(v)}
+}
+*/
+class TyoisDes {
+    static some(v) {return Tys.name(v).startsWith('Descriptor<')}
+    static get d() {return TyoisDesD}
+    static get a() {return TyoisDesA}
+}
+
+class TyoisDesDA {
+    static some(v, names) {
+        const N = Tys.name(v);
+//        const ns = 'Value Method'.split(' ').map(n=>`Descriptor<${n}>`);
+//        return ns.some(n=>n===N);
+        const ns = names.map(n=>`Descriptor<${n}>`);
+        return ns.some(n=>n===N);
+    }
+}
+// Data/Accessor
+class TyoisDesD extends TyoisDesDA {
+    //static some(v) {return Tys.name(v).startsWith('Descriptor<')}
+    /*
+    static some(v) {
+        const N = Tys.name(v);
+        const ns = 'Value Method'.split(' ').map(n=>`Descriptor<${n}>`);
+        return ns.some(n=>n===N);
+    }
+    */
+    static some(v) {return super.some(v, 'Value Method'.split(' '))}
+    static v(v) {return 'Descriptor<Value>'===Tys.name(v)}
+    static m(v) {return 'Descriptor<Method>'===Tys.name(v)}
+}
+class TyoisDesA extends TyoisDesDA {
+//    static some(v) {return Tys.name(v).startsWith('Descriptor<')}
+/*
+    static some(v) {
+        const N = Tys.name(v);
+        const ns = 'Getter Setter Accessor'.split(' ').map(n=>`Descriptor<${n}>`);
+        return ns.some(n=>n===N);
+    }
+*/
+    static some(v) {return super.some(v, 'Getter Setter Accessor'.split(' '))}
+    static g(v) {return 'Descriptor<Getter>'===Tys.name(v)}
+    static s(v) {return 'Descriptor<Setter>'===Tys.name(v)}
+    static a(v) {return 'Descriptor<Accessor>'===Tys.name(v)}
+}
+
+class TyoisFn {
+    static some() {return Tys.name(v).endsWith('Function')}
+    static bound() {return 'BoundFunction'===Tys.name(v)}
+    static native() {return 'NativeFunction'===Tys.name(v)}
+    static get arrow() {return TyoisArrFn}
+    static a() {return 'AsyncFunction'===Tys.name(v)}
+    static g() {return 'GeneratorFunction'===Tys.name(v)}
+    static ag() {return 'AsyncGeneratorFunction'===Tys.name(v)}
+    static s() {return 'Function'===Tys.name(v)}
+}
+class TyoisArrFn {
+    static some() {return Tys.name(v).endsWith('ArrowFunction')}
+    static a() {return 'AsyncArrowFunction'===Tys.name(v)}
+    static s() {return 'ArrowFunction'===Tys.name(v)}
+}
+class TyoisMd {
+    static some() {return Tys.name(v).endsWith('Method')}
+    static a() {return 'AsyncMethod'===Tys.name(v)}
+    static g() {return 'GeneratorMethod'===Tys.name(v)}
+    static ag() {return 'AsyncGeneratorMethod'===Tys.name(v)}
+    static s() {return 'Method'===Tys.name(v)}
+}
+// Error
+class Tyoer {
+    static some(v) {
+        if (Tyois.some(v)) return true;
+        throw new TypeError(`Expected: a value that makes 'Tyois.some(v)' return true.\nActual: ${Tys.name(v)}`);
+    }
+    static obj(v) {return this._('obj', v);}
+    static ary(v) {return this._('ary', v);}
+    static cls(v) {return this._('cls', v);}
+    static ins(v,C) {return this._('ins', v);}
+    static get des() {return TyoerDes}
+    static get fn() {return TyoerFn}
+    static _(n,v) {
+        if (Tyois[n](v)) return true;
+        throw new TyoeError(`Expected: '${Tyois[n].toString()}' like value.\nActual: ${Tys.name(v)}`);
+    }
+}
+class TyoerDes {
+    static some(v) {
+        if (TyoisDes.some(v)) return true;
+        throw new TypeError(`Expected: a value that makes 'Tyois.des.some(v)' return true.\nActual: ${Tys.name(v)}`);
+    }
+    static v(v) {return }
+    static m(v) {return }
+    static g(v) {return }
+    static s(v) {return }
+    static a(v) {return }
+    static _(n,v) {
+        if (TyoisDes[n](v)) return true;
+        throw new TyoeError(`Expected: '${TyoisDes[n].toString()}' like value.\nActual: ${Tys.name(v)}`);
+    }
+}
+class TyoerFn {
+    static some(v) {
+        if (TyoisFn.some(v)) return true;
+        throw new TypeError(`Expected: a value that makes 'Tyois.fn.some(v)' return true.\nActual: ${Tys.name(v)}`);
+    }
+    static bound() {return this._('bound',v)}
+    static native() {return this._('native',v)}
+    static get arrow() {return TyoerArrFn}
+    static a() {return this._('a',v)}
+    static g() {return this._('g',v)}
+    static ag() {return this._('ag',v)}
+    static s() {return this._('s',v)}
+    static _(n,v) {
+        if (TyoisFn[n](v)) return true;
+        throw new TyoeError(`Expected: '${TyoisFn[n].toString()}' like value.\nActual: ${Tys.name(v)}`);
+    }
+}
+class TyoerArrFn {
+    static some(v) {
+        if (TyoisArrFn.some(v)) return true;
+        throw new TypeError(`Expected: a value that makes 'Tyois.fn.arrow.some(v)' return true.\nActual: ${Tys.name(v)}`);
+    }
+    static a() {return this._('a',v)}
+    static s() {return this._('s',v)}
+    static _(n,v) {
+        if (TyoisArrFn[n](v)) return true;
+        throw new TyoeError(`Expected: '${TyoisArrFn[n].toString()}' like value.\nActual: ${Tys.name(v)}`);
+    }
+}
---
+++ docs/11/test/tyo.js
@@ -0,0 +1,319 @@
+import { expect, test, describe } from "bun:test";
+import {Tyo} from '../src/tyo.js';
+import {Tys} from '../src/tys.js';
+/**
+ * 指定した関数を実行し、期待する例外型とメッセージが完全に一致してスローされるか検証する
+ * @param fn 検証対象の関数
+ * @param expectedErrorClass 期待する例外のコンストラクタ（例: TyoeError, CustomError など）
+ * @param expectedMessage 期待する完全一致のエラーメッセージ
+ */
+function assertThrow(Err, msg, fn) {
+    let err = null;
+    try {fn();} catch (error) {err = error;}
+    expect(err).not.toBeNull();
+    expect(err).toBeInstanceOf(Err);
+    expect(err.message).toBe(msg);
+}
+// テストデータ
+class C {
+    static get sg() {}
+    static set ss(v) {}
+    static get sa() {}
+    static set sa(v) {}
+    get g() {}
+    set s(v) {}
+    get a() {}
+    set a(v) {}
+    static sm(){}
+    static *sgm(){}
+    static async sam(){}
+    static async *sagm(){}
+    m(){}
+    *gm(){}
+    async am(){}
+    async *agm(){}
+}
+const c = new C();
+function fn(){}
+function *gfn(){}
+async function afn(){}
+async function *agfn(){}
+const arrFn = ()=>{};
+const aarrFn = async()=>{};
+const getDes = (o,d)=>Object.getOwnPropertyDescriptor(Object.defineProperty(o, 'd', d), 'd');
+const des = {
+    o: [[{}, {value:0}], [{}, {value(){}}], [{_d:0}, {get(){return this._d}}], [{_d:0}, {set(v){this._d=v;}}], [{_d:0}, {get(){return this._d}, set(v){this._d=v}}]].map(x=>[getDes(...x)]),
+    c: [[C,'sg'],[C,'ss'],[C,'sa']].map(x=>[Object.getOwnPropertyDescriptor(x[0], x[1])]),
+    i: [[c,'g'],[c,'s'],[c,'a']].map(x=>[Object.getOwnPropertyDescriptor(Object.getPrototypeOf(x[0]), x[1])]),
+};
+const cal = {
+    fn: [[fn],[gfn],[afn],[agfn],[arrFn],[aarrFn],[function(){}],[function*(){}],[async function(){}],[async function*(){}],[()=>{}],[async()=>{}]],
+    md: [[C.sm],[C.sgm],[C.sam],[C.sagm],[c.m],[c.gm],[c.am],[c.agm]],
+}
+//const prims = [[undefined],[null],[NaN],[Infinity],[-Infinity],[new Boolean()],[new Number()],[new String()],[Object.create(null)],[true],[false],[0],[Number.MAX_SAFE_INTEGER],[Number.MIN_SAFE_INTEGER],[0.1],[0n],[''],[Symbol()]];
+const prims = [[true],[false],[0],[Number.MAX_SAFE_INTEGER],[Number.MIN_SAFE_INTEGER],[0.1],[0n],[''],[Symbol()]];
+const objs = [[{}],[[]],[C],[class{}],[class C{}],[new C()],[new (class{})],[new (class C{})], ...des.o, ...des.c, ...des.i, ...cal.fn, ...cal.md];
+const dangers = [[undefined],[null],[NaN],[Infinity],[-Infinity],[new Boolean()],[new Number()],[new String()],[Object.create(null)],[Object.create(null)],[new fn()]];
+describe(`Tyo`, ()=>{
+    describe(`is`, ()=>{
+        describe(`some`, ()=>{
+            describe(`false`, ()=>{
+                test.each([...prims, ...dangers])(`(%p)`, (v)=>{
+                    expect(Tyo.is.some(v)).toBe(false);
+                });
+            });
+            describe(`true`, ()=>{
+                test.each(objs)(`(%p)`, (v)=>{
+                    expect(Tyo.is.some(v)).toBe(true);
+                });
+                describe(`Descriptor系`, ()=>{
+                    describe(`Obj系`, ()=>{
+                        test.each(des.o)(`(%p)`, (v)=>expect(Tyo.is.some(v)).toBe(true));
+                    });
+                    describe(`Cls系`, ()=>{
+                        test.each(des.c)(`(%p)`, (v)=>expect(Tyo.is.some(v)).toBe(true));;
+                    });
+                    describe(`Ins系`, ()=>{
+                        test.each(des.i)(`(%p)`, (v)=>expect(Tyo.is.some(v)).toBe(true));;
+                     });
+                });
+                describe(`Fn系`, ()=>{
+                    test.each(cal.fn)(`(%p)`, (v)=>expect(Tyo.is.some(v)).toBe(true));
+                });
+                describe(`Method系`, ()=>{
+                    test.each(cal.md)(`(%p)`, (v)=>expect(Tyo.is.some(v)).toBe(true));
+                });
+            });
+        });
+        describe(`obj`, ()=>{
+            describe(`false`, ()=>{
+                test.each([[[]],[C],[class{}],[class C{}],[new C()],[new (class{})],[new (class C{})], ...dangers, ...prims, ...des.o, ...des.c, ...des.i, ...cal.fn, ...cal.md])(`(%p)`, (v)=>{
+                    expect(Tyo.is.obj(v)).toBe(false);
+                });
+            });
+            describe(`true`, ()=>{
+                test.each([[{}]])(`(%p)`, (v)=>{
+                    expect(Tyo.is.obj(v)).toBe(true);
+                });
+            });
+        });
+        describe(`ary`, ()=>{
+            describe(`false`, ()=>{
+                test.each([[{}],[C],[class{}],[class C{}],[new C()],[new (class{})],[new (class C{})], ...dangers, ...prims, ...des.o, ...des.c, ...des.i, ...cal.fn, ...cal.md])(`(%p)`, (v)=>{
+                    expect(Tyo.is.ary(v)).toBe(false);
+                });
+            });
+            describe(`true`, ()=>{
+                test.each([[[]],[[1,2,'a']]])(`(%p)`, (v)=>{
+                    expect(Tyo.is.ary(v)).toBe(true);
+                });
+            });
+        });
+        describe(`cls`, ()=>{
+            describe(`false`, ()=>{
+                test.each([[{}],[[]],[new C()],[new (class{})],[new (class C{})], ...dangers, ...prims, ...des.o, ...des.c, ...des.i, ...cal.fn, ...cal.md])(`(%p)`, (v)=>{
+                    expect(Tyo.is.cls(v)).toBe(false);
+                });
+            });
+            describe(`true`, ()=>{
+                test.each([[C],[class{}],[class C{}]])(`(%p)`, (v)=>{
+                    expect(Tyo.is.cls(v)).toBe(true);
+                });
+            });
+        });
+        describe(`ins`, ()=>{
+            describe(`false`, ()=>{
+                test.each([[{}],[[]],[C],[class{}],[class C{}], ...dangers, ...prims, ...des.o, ...des.c, ...des.i, ...cal.fn, ...cal.md])(`(%p)`, (v)=>{
+                    expect(Tyo.is.ins(v)).toBe(false);
+                });
+            });
+            describe(`true`, ()=>{
+                test.each([[new C()],[new (class{})],[new (class C{})]])(`(%p)`, (v)=>{
+                    expect(Tyo.is.ins(v)).toBe(true);
+                });
+            });
+        });
+        describe(`des`, ()=>{
+            describe(`some`, ()=>{
+                describe(`false`, ()=>{
+                    test.each([[{}],[[]],[C],[class{}],[class C{}],[new C()],[new (class{})],[new (class C{})], ...dangers, ...prims, ...cal.fn, ...cal.md])(`(%p)`, (v)=>{
+                        expect(Tyo.is.des.some(v)).toBe(false);
+                    });
+                });
+                describe(`true`, ()=>{
+                    test.each(...des.o, ...des.c, ...des.i)(`(%p)`, (v)=>{
+                        expect(Tyo.is.des.some(v)).toBe(true);
+                    });
+                });
+            });
+            describe(`d`, ()=>{
+                describe(`some`, ()=>{
+                    describe(`false`, ()=>{
+                        describe(`Obj系`, ()=>{
+                            const data = [[{_d:0}, {get(){return this._d}}], [{_d:0}, {set(v){this._d=v;}}], [{_d:0}, {get(){return this._d}, set(v){this._d=v}}]].map(x=>[getDes(...x)]);
+                            test.each(data)(`(%p)`, (v)=>{
+                                expect(Tyo.is.des.d.some(v)).toBe(false);
+                            });
+                        });
+                        describe(`Cls系`, ()=>{
+                            test.each(des.c)(`(%p)`, (v)=>{
+                                expect(Tyo.is.des.d.some(v)).toBe(false);
+                            });
+                        });
+                        describe(`Ins系`, ()=>{
+                            test.each(des.i)(`(%p)`, (v)=>{
+                                expect(Tyo.is.des.d.some(v)).toBe(false);
+                            });
+                        });
+                    });
+                    describe(`true`, ()=>{
+                        describe(`Obj系`, ()=>{
+                            const data = [[{}, {value:0}], [{}, {value(){}}]].map(x=>[getDes(...x)])
+                            test.each(data)(`(%p)`, (v)=>{
+                                expect(Tyo.is.des.d.some(v)).toBe(true);
+                            });
+                        });
+                    });
+                });
+                describe(`v`, ()=>{
+                    describe(`false`, ()=>{
+                        describe(`Obj系`, ()=>{
+                            const data = [[{}, {value(){}}], [{_d:0}, {get(){return this._d}}], [{_d:0}, {set(v){this._d=v;}}], [{_d:0}, {get(){return this._d}, set(v){this._d=v}}]].map(x=>[getDes(...x)]);
+                            test.each(data)(`(%p)`, (v)=>{
+                                expect(Tyo.is.des.d.v(v)).toBe(false);
+                            });
+                        });
+                        describe(`Cls系`, ()=>{
+                            test.each(des.c)(`(%p)`, (v)=>{
+                                expect(Tyo.is.des.d.v(v)).toBe(false);
+                            });
+                        });
+                        describe(`Ins系`, ()=>{
+                            test.each(des.i)(`(%p)`, (v)=>{
+                                expect(Tyo.is.des.d.v(v)).toBe(false);
+                            });
+                        });
+                    });
+                    describe(`true`, ()=>{
+                        describe(`Obj系`, ()=>{
+                            const data = [[{}, {value:0}]].map(x=>[getDes(...x)])
+                            test.each(data)(`(%p)`, (v)=>{
+                                expect(Tyo.is.des.d.v(v)).toBe(true);
+                            });
+                        });
+                    });
+                });
+                describe(`m`, ()=>{
+                    describe(`false`, ()=>{
+                        describe(`Obj系`, ()=>{
+                            const data = [[{}, {value:0}], [{_d:0}, {get(){return this._d}}], [{_d:0}, {set(v){this._d=v;}}], [{_d:0}, {get(){return this._d}, set(v){this._d=v}}]].map(x=>[getDes(...x)]);
+                            test.each(data)(`(%p)`, (v)=>{
+                                expect(Tyo.is.des.d.m(v)).toBe(false);
+                            });
+                        });
+                        describe(`Cls系`, ()=>{
+                            test.each(des.c)(`(%p)`, (v)=>{
+                                expect(Tyo.is.des.d.m(v)).toBe(false);
+                            });
+                        });
+                        describe(`Ins系`, ()=>{
+                            test.each(des.i)(`(%p)`, (v)=>{
+                                expect(Tyo.is.des.d.m(v)).toBe(false);
+                            });
+                        });
+                    });
+                    describe(`true`, ()=>{
+                        describe(`Obj系`, ()=>{
+                            const data = [[{}, {value(){}}]].map(x=>[getDes(...x)])
+                            test.each(data)(`(%p)`, (v)=>{
+                                expect(Tyo.is.des.d.m(v)).toBe(true);
+                            });
+                        });
+                    });
+                });
+            });
+
+/*
+const getDes = (o,d)=>Object.getOwnPropertyDescriptor(Object.defineProperty(o, 'd', d), 'd');
+const des = {
+    o: [[{}, {value:0}], [{}, {value(){}}], [{_d:0}, {get(){return this._d}}], [{_d:0}, {set(v){this._d=v;}}], [{_d:0}, {get(){return this._d}, set(v){this._d=v}}]].map(x=>[getDes(...x)]),
+    c: [[C,'sg'],[C,'ss'],[C,'sa']].map(x=>[Object.getOwnPropertyDescriptor(x[0], x[1])]),
+    i: [[c,'g'],[c,'s'],[c,'a']].map(x=>[Object.getOwnPropertyDescriptor(Object.getPrototypeOf(x[0]), x[1])]),
+};
+*/
+            describe(`a`, ()=>{
+                describe(`some`, ()=>{
+
+                });
+                describe(`g`, ()=>{
+                    describe(`false`, ()=>{
+                        describe(`Obj系`, ()=>{
+                            const data = [[{}, {value(){}}],[{}, {value:0}],[{_d:0}, {set(v){this._d=v;}}], [{_d:0}, {get(){return this._d}, set(v){this._d=v}}]].map(x=>[getDes(...x)]);
+                            test.each(data)(`(%p)`, (v)=>{
+                                expect(Tyo.is.des.a.g(v)).toBe(false);
+                            });
+                        });
+                        describe(`Cls系`, ()=>{
+                            const data = [[C,'ss'],[C,'sa']].map(x=>[Object.getOwnPropertyDescriptor(x[0], x[1])]);
+                            test.each(data)(`(%p)`, (v)=>{
+                                expect(Tyo.is.des.a.g(v)).toBe(false);
+                            });
+                        });
+                        describe(`Ins系`, ()=>{
+                            const data = [[c,'s'],[c,'a']].map(x=>[Object.getOwnPropertyDescriptor(Object.getPrototypeOf(x[0]), x[1])]);
+                            test.each(data)(`(%p)`, (v)=>{
+                                expect(Tyo.is.des.a.g(v)).toBe(false);
+                            });
+                        });
+                    });
+                    describe(`true`, ()=>{
+                        describe(`Obj系`, ()=>{
+                            const data = [[{_d:0}, {get(){return this._d}}]].map(x=>[getDes(...x)])
+                            test.each(data)(`(%p)`, (v)=>{
+                                expect(Tyo.is.des.a.g(v)).toBe(true);
+                            });
+                        });
+                        describe(`Cls系`, ()=>{
+                            const data = [[C,'sg']].map(x=>[Object.getOwnPropertyDescriptor(x[0], x[1])]);
+                            test.each(data)(`(%p)`, (v)=>{
+                                expect(Tyo.is.des.a.g(v)).toBe(true);
+                            });
+                        });
+                        describe(`Ins系`, ()=>{
+                            const data = [[c,'g']].map(x=>[Object.getOwnPropertyDescriptor(Object.getPrototypeOf(x[0]), x[1])]);
+                            test.each(data)(`(%p)`, (v)=>{
+                                expect(Tyo.is.des.a.g(v)).toBe(true);
+                            });
+                        });
+                    });
+                });
+            });
+        });
+        describe(`fn`, ()=>{
+            describe(`false`, ()=>{
+            });
+            describe(`true`, ()=>{
+            });
+        });
+        describe(`md`, ()=>{
+            describe(`false`, ()=>{
+            });
+            describe(`true`, ()=>{
+            });
+        });
+    });
+    describe(`er`, ()=>{
+        describe(`some`, ()=>{
+            describe(`false`, ()=>{
+                test.each([...prims, ...dangers])(`(%p)`, (v)=>{
+                    assertThrow(TypeError, `Expected: a value that makes 'Tyois.some(v)' return true.\nActual: ${Tys.name(v)}`, ()=>Tyo.er.some(v));
+                });
+            });
+            describe(`true`, ()=>{
+                test.each(objs)(`(%p)`, (v)=>{
+                    expect(Tyo.er.some(v)).toBe(true);
+                });
+            });
+        });
+    });
+});
