f558caa830d3f917f3117d5b7c33882017db9c72
ytyaru <yttry0@gmail.com>
2026-08-30 11:43:47 +0900

    add 27

---
+++ docs/27/src/main.js
@@ -0,0 +1,25 @@
+import {Tys} from './tys.js';
+import {Typis} from './typ.js'; // クラスではなく判定関数そのものをインポート
+import {Tyois} from './tyo.js';
+import {Tydis} from './tyd.js';
+import {FnObj} from './fn-obj.js';
+
+const owTp = FnObj.mkEr(Typis, 'isT.p.some(v)');
+const owTo = FnObj.mkEr(Tyois, 'isT.o.some(v)');
+const owTd = FnObj.mkEr(Tydis, 'isT.d.some(v)');
+
+class isT {
+    static get p() {return Typis;}
+    static get o() {return Tyois;}
+    static get d() {return Tydis;}
+}
+
+class owT {
+    static get p() {return owTp;}
+    static get o() {return owTo;}
+    static get d() {return owTd;}
+}
+
+const tof = v => Tys.name(v);
+
+export {isT, owT, tof};
